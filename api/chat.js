836957f5c00526fce38b076759cc57784240c4dc
Marxob <eperformance.it@gmail.com>
2026-04-17 15:23:40 +0200

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -39,6 +39,16 @@ async function getSpettacoli() {
 }
 
 export default async function handler(req, res) {
+// CORS fix
+res.setHeader("Access-Control-Allow-Origin", "*");
+res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+// gestisce preflight
+if (req.method === "OPTIONS") {
+  return res.status(200).end();
+}
+
   try {
     if (req.method !== "POST") {
       return res.status(405).send("Method not allowed");
